13e0c71a7a67adc992888ac5739e2b7bdb3339af
Jordan Harrison <wgsinners@gmail.com>
2015-08-15 12:58:20 -0400

    Basic homepage theming for slideshow, headers, blocks

--- sites/all/themes/at2015/template.php
+++ sites/all/themes/at2015/template.php
@@ -135,12 +135,35 @@ function at2015_preprocess_block(&$variables, $hook) {
 
 function at2015_preprocess_html(&$variables) {
 drupal_add_css('https://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css', array('group' => CSS_THEME, 'preprocess' => FALSE));
-drupal_add_css('https://fonts.googleapis.com/css?family=Raleway', array('group' => CSS_THEME, 'preprocess' => FALSE));
+drupal_add_css('https://fonts.googleapis.com/css?family=Fira_Sans', array('group' => CSS_THEME, 'preprocess' => FALSE));
 }
 
 function hook_preprocess_page(&$vars) {
 drupal_add_js('https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js', 'external');
 }
+
+
+/**
+* hook_form_FORM_ID_alter
+*/
+function at2015_form_search_block_form_alter(&$form, &$form_state, $form_id) {
+    $form['search_block_form']['#title'] = t('Search'); // Change the text on the label element
+    $form['search_block_form']['#title_display'] = 'invisible'; // Toggle label visibilty
+    $form['search_block_form']['#size'] = 30;  // define size of the textfield
+    //$form['search_block_form']['#default_value'] = t('Search...'); // Set a default value for the textfield
+    //$form['actions']['submit']['#value'] = t('GO!'); // Change the text on the submit button
+    //$form['actions']['submit'] = array('#type' => 'image_button', '#src' => base_path() . path_to_theme() . '/images/search-button.png');
+
+    // Add extra attributes to the text box
+    $form['search_block_form']['#attributes']['onblur'] = "if (this.value == '') {this.value = 'Search';}";
+    $form['search_block_form']['#attributes']['onfocus'] = "if (this.value == 'Search') {this.value = '';}";
+    // Prevent user from searching the default text
+    $form['#attributes']['onsubmit'] = "if(this.search_block_form.value=='Search'){ alert('Please enter a search'); return false; }";
+
+    // Alternative (HTML5) placeholder attribute instead of using the javascript
+    $form['search_block_form']['#attributes']['placeholder'] = t('Search');
+} 
+
 
 /**
  * Theme the calendar title
