102ffa9b4b35c556ee7525a5b2319416f6ed770a
Jordan Harrison <wgsinners@gmail.com>
2015-08-15 15:59:32 -0400

    Slideshow controls

--- sites/all/modules/views_slideshow/theme/views-slideshow-controls-text-next.tpl.php
+++ sites/all/modules/views_slideshow/theme/views-slideshow-controls-text-next.tpl.php
@@ -1,1 +1,5 @@
-<span id="views_slideshow_controls_text_next_<?php print $variables['vss_id']; ?>" class="<?php print $classes; ?>"><a href="#"><?php print t('Next'); ?></a></span>
+<span id="views_slideshow_controls_text_next_<?php print $variables['vss_id']; ?>" class="<?php print $classes; ?>">
+	<a href="#"><span class="fa-stack fa-lg">
+  <i class="fa fa-circle fa-stack-2x"></i>
+  <i class="fa fa-chevron-right fa-stack-1x"></i>
+</span></a></span>
