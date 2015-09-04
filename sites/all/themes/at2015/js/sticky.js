158d85a6ad59153d9b55a15cd3f49c156d75805f
Gregg Shanefelt <grshane@gmail.com>
2015-09-03 23:09:08 -0400

    Sticky Nav fit

--- sites/all/themes/at2015/js/sticky.js
+++ sites/all/themes/at2015/js/sticky.js
@@ -1,15 +1,20 @@
 $(function () {
     //store the element
-    var $top = $('.header');
+    var $top = $('header');
+    var $top2 = $('.navigation');
 
     //store the initial position of the element
     $(window).scroll(function(){
         if($(document).scrollTop() > 100) {
           // if so, add the fixed class
           $top.addClass('fixed');
+          $top.css('display:block');
+          $top2.addClass('fixed');
+          $top2.css('top: 50px');
         } else {
           // otherwise remove it
-          $top.removeClass('fixed');
+          $top2.removeClass('fixed');
+
         }
     });
 });
