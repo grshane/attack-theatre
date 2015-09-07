368061290cffc22d0f67d379dbc568f6505b863a
Gregg Shanefelt <grshane@gmail.com>
2015-09-07 16:43:15 -0400

    Add devel and wrapper for page.tpl

--- sites/all/themes/at2015/templates/page.tpl.php
+++ sites/all/themes/at2015/templates/page.tpl.php
@@ -21,20 +21,21 @@
     <?php print render($page['highlighted']); ?>
 
   </div>
-  
+
   <div id="main">
+<div id="content-container">
 <div class="title_bar">
 <?php print render($title_prefix); ?>
       <?php if ($title): ?>
         <h1 class="page__title title" id="page-title"><?php print $title; ?></h1>
       <?php endif; ?>
       <?php print render($title_suffix); ?>
-  </div>    
+  </div>
     <div id="content" class="column" role="main">
 
-     
+
       <a id="main-content"></a>
-      
+
       <?php print $messages; ?>
       <?php print render($tabs); ?>
       <?php print render($page['help']); ?>
@@ -45,7 +46,7 @@
       <?php print $feed_icons; ?>
     </div>
 
-    
+
 
     <?php
       // Render the sidebars to see if there's anything in them.
@@ -59,7 +60,7 @@
         <?php print $sidebar_second; ?>
       </aside>
     <?php endif; ?>
-
+</div>
   </div>
 <?php print render($page['footer']); ?>
 
