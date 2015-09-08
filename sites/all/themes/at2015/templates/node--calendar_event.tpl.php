3f5803af99773d8f58b73bc55c7c68ee8fa296bb
Gregg Shanefelt <grshane@gmail.com>
2015-09-07 23:59:10 -0400

    Add press link

--- sites/all/themes/at2015/templates/node--calendar_event.tpl.php
+++ sites/all/themes/at2015/templates/node--calendar_event.tpl.php
@@ -10,18 +10,18 @@
 
 <article class="node-<?php print $node->nid; ?> <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
-  <?php if ($title_prefix || $title_suffix || $display_submitted || $unpublished || !$page && $title): ?>
+  <?php /*if ($title_prefix || $title_suffix || $display_submitted || $unpublished || !$page && $title): */?>
 
     <div class="event_buttons">
 <?php
  dsm($node);
 ?>
 
-<?php
-  if ($output != null): ?>
+
+ <?php if (!empty($content['field_showclix_link'])): ?>
       <?php echo '<a href="'.render($content['field_showclix_link']).'&width=600&height=600&iframe=true"><span class="button">Tickets</span></a>';?>
       <?php else: ?>
-      <?php echo ''; ?>
+      <?php echo 'nothing found'; ?>
     <?php endif; ?>
 
     <?php if (!empty($content['field_press_citations'])): ?>
@@ -33,7 +33,7 @@
 <?php print render($content['addtoany']); ?>
 
 </div>
-  <?php endif; ?>
+  <?php /*endif; */?>
 
   <?php
     // We hide the comments and links now so that we can render them later.
