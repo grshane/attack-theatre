6ce7ae4564166a2fd0edb01aacfc1aefb59eb680
Gregg Shanefelt <grshane@gmail.com>
2015-09-07 23:40:30 -0400

    Add dsm node dump for testing

--- sites/all/themes/at2015/templates/node--calendar_event.tpl.php
+++ sites/all/themes/at2015/templates/node--calendar_event.tpl.php
@@ -11,24 +11,28 @@
 <article class="node-<?php print $node->nid; ?> <?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
   <?php if ($title_prefix || $title_suffix || $display_submitted || $unpublished || !$page && $title): ?>
-    <header>
-      <?php print render($title_prefix); ?>
-      <?php if (!$page && $title): ?>
-        <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
 
-      <?php if ($display_submitted): ?>
-        <p class="submitted">
-          <?php print $user_picture; ?>
-          <?php print $submitted; ?>
-        </p>
-      <?php endif; ?>
+    <div class="event_buttons">
+<?php
+ dsm($node);
+?>
 
-      <?php if ($unpublished): ?>
-        <mark class="unpublished"><?php print t('Unpublished'); ?></mark>
-      <?php endif; ?>
-    </header>
+<?php
+  if ($output != null): ?>
+      <?php echo '<a href="'.render($content['field_showclix_link']).'&width=600&height=600&iframe=true"><span class="button">Tickets</span></a>';?>
+      <?php else: ?>
+      <?php echo ''; ?>
+    <?php endif; ?>
+
+    <?php if (!empty($content['field_press_citations'])): ?>
+      <?php echo '<a href="#press">Press</a>';?>
+      <?php else: ?>
+      <?php echo ''; ?>
+    <?php endif; ?>
+
+<?php print render($content['addtoany']); ?>
+
+</div>
   <?php endif; ?>
 
   <?php
