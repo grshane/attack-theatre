e901e663dd35c9e715ebba01d5bc35154f2ee224
Gregg Shanefelt <grshane@gmail.com>
2015-09-22 20:52:21 -0400

    Add javascript to add header ctas

--- sites/all/themes/at2015/js/sticky.js
+++ sites/all/themes/at2015/js/sticky.js
@@ -2,6 +2,7 @@ $(function () {
     //store the element
     var $top = $('.main__nav');
     var $top2 = $('.navigation');
+    var $ctaButton = $('.header');
 
     //store the initial position of the element
     $(window).scroll(function(){
@@ -11,6 +12,7 @@ $(function () {
           $top.css('display:block');
           $top2.addClass('fixed');
           $top2.css('top: 50px');
+          $ctaButton.addClass('fixed');
         } else {
           // otherwise remove it
           $top2.removeClass('fixed');
